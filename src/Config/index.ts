0d6a32446a50574ad099544d1470951372e44d51
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2022-12-12 20:58:31 +0100

    connection to database

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,9 +1,13 @@
-import {Sequelize} from 'sequelize';
-import dotenv from 'dotenv'
-dotenv.config()
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const db = new Sequelize('app', '', '',{
-    storage:"",
-    dialect:"postgres",
-    logging:false
-})
+dotenv.config();
+
+export const db = new Sequelize("postgres", "postgres", "1234", {
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  logging: false,
+});
+
+export const dbPassword = process.env.DB_PASSWORD;
+export const authToken = process.env.AUTHTOKEN;
