0014b7672b6e6dde9c592303df51360ebc8abead
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-12-13 13:33:58 +0100

    fixed no sequelize instance passed

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 export const accountSid = process.env.AccountSID;
 export const authToken = process.env.AuthToken;
 export const fromAdminPhone = process.env.fromAdminPhone;
-export const APP_SECRET = process.env.APP_SECRET as string; 
+export const APP_SECRET = process.env.APP_SECRET as string;
 export const FromAdminMail = process.env.FromAdminMail as string;
 export const userSubject = process.env.usersubject as string;
 export const GMAIL_USER = process.env.GMAIL_USER;
 export const GMAIL_PASS = process.env.GMAIL_PASS;
 dotenv.config();
 
-const db = new Sequelize("postgres", "postgres", "root", {
+export const db = new Sequelize("postgres", "postgres", "root", {
   host: process.env.DB_HOST,
   dialect: "postgres",
   logging: false,
@@ -26,4 +26,3 @@ export const connectDB = async () => {
     console.error("Unable to connect to the database:", error);
   }
 };
-
