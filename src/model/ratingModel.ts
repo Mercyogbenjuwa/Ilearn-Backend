d674ba82fc07a5f39e998fb8bed74d907f4e3e90
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2022-12-15 23:44:37 +0100

    ratingmodel

---
+++ src/model/ratingModel.ts
@@ -0,0 +1,68 @@
+import { Sequelize, Model, DataTypes } from "sequelize";
+import { db } from "../Config/index";
+
+export interface RatingAttributes {
+  [x: string]: any;
+  id: string;
+  title: string;
+  description: string;
+  courseId: string;
+  studentId: string;
+  ratingValue: number;
+  
+}
+
+export class RatingInstance extends Model<RatingAttributes> {
+  declare id: string;
+  declare title: string;
+  declare description: string;
+  declare courseId: string;
+  declare studentId: string;
+  declare ratingValue: number;
+  
+}
+
+RatingInstance.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      // defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    
+      },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      
+    },
+    courseId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    
+    studentId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    ratingValue: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    
+  },
+
+  {
+    sequelize: db,
+    tableName: "rating",
+  }
+);
