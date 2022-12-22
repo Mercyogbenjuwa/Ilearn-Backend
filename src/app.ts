be62ba41f35bf34b4eabf2b63db7bf1c10d037be
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-12-22 18:41:50 +0100

    added env sample

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(logger("dev"));
 app.use(cookieParser());
