97a48525708c666a4ea03452b2e0367237f000f7
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-12-23 13:58:56 +0100

    tested to be working with cloudinary

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,26 +1,23 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async (req, file) => {
-        return {
-            folder: 'ILEARN',
-        }
-    },
-
-    
-
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: process.env.CLOUDINARY_DIRECTORY,
+    };
+  },
 });
 
 // const fileFilter = (req: any, file: any, cb: any) => {
@@ -31,11 +28,8 @@ const storage = new CloudinaryStorage({
 //     }
 // }
 
-
- export const upload = multer({ 
-    storage: storage, 
-//    fileFilter: fileFilter,
-//     limits: {fieldSize: 1024 * 1024 * 5} 
+export const upload = multer({
+  storage: storage,
+  //    fileFilter: fileFilter,
+  //     limits: {fieldSize: 1024 * 1024 * 5}
 });
-
- 
